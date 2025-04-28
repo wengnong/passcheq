0fce18dfa9180404677ff10078385758ec171430
Winona <155759283+wengnong@users.noreply.github.com>
2025-04-28 21:41:32 +0700

    Developed frontend interfaces

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,18 @@
-const App = () => {
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import GeneratePage from './pages/GeneratePage'
+import HistoryPage from './pages/HistoryPage'
+import CheckerPage from './pages/CheckerPage'
+
+const App: React.FC = () => {
   return (
-    <div className="text-4xl font-bold bg-blue-200">Testing</div>
-  )
-}
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/generate-password" element={<GeneratePage />} />
+      <Route path="/history-password" element={<HistoryPage />} />
+      <Route path="/check-password" element={<CheckerPage />} />
+    </Routes>
+  );
+};
 
 export default App
--- client/src/pages/CheckerPage.tsx
+++ client/src/pages/CheckerPage.tsx
@@ -1,7 +1,14 @@
-const CheckerPage = () => {
+const CheckerPage: React.FC = () => {
     return (
-        <div>CheckerPage</div>
-    )
-}
+        <div>
+            CheckerPage
+            {/* 
+                Di fitur password checker, tambahin library password-password yang sering bocor (dataset).
+                Semisalnya passwordnya yang kita cek itu lemah, kita bisa kasih tau kenapa password
+                tersebut itu lemah.
+            */}
+        </div>
+    );
+};
 
 export default CheckerPage
